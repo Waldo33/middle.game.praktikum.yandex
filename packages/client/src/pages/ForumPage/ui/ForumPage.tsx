f0fe50e248b0219a2e17fff459cbae6c95fd3b81
waldo <evgenijtser@gmail.com>
2024-10-15 23:54:06 +0300

    Create pages stubs

---
+++ packages/client/src/pages/ForumPage/ui/ForumPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+export const ForumPage: FC = () => {
+  // throw new Error('123')
+
+  return <div>Форум</div>
+}
---
+++ packages/client/src/pages/LeaderboardPage/ui/LeaderboardPage.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react'
+
+export const LeaderboardPage: FC = () => {
+  return <div>Лидерборд</div>
+}
