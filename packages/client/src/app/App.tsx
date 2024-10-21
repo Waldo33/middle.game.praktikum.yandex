3ffdcc1d351f8d01a74fc6c1daf6dca1e5c423a4
collusion <51285072+galnikolai@users.noreply.github.com>
2024-10-21 21:11:50 +0300

    readable

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -3,7 +3,7 @@ import { Outlet, useLoaderData } from 'react-router-dom'
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { Toaster } from '../shared/components/ui/toaster'
+import { Toaster } from '@shared/components/ui/toaster'
 import { AppDispatch } from './store'
 import { User, setUser, setError } from '@processes/auth/model/authSlice'
 import { LoadingSpinner } from '@shared/components/ui/loading-spinner'
@@ -11,8 +11,8 @@ import { selectAuthLoading } from '@processes/auth/model/selectors'
 
 export const App = () => {
   const dispatch: AppDispatch = useDispatch()
-  const userData = useLoaderData() as User | null
-  const loading = useSelector(selectAuthLoading)
+  const userData: User | null | unknown = useLoaderData()
+  const authLoading = useSelector(selectAuthLoading)
 
   useEffect(() => {
     if (userData) {
@@ -24,7 +24,8 @@ export const App = () => {
 
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      {loading ? <LoadingSpinner /> : <Outlet />}
+      {authLoading && <LoadingSpinner />}
+      {!authLoading && <Outlet />}
       <Toaster />
     </div>
   )
--- packages/client/src/app/store.ts
+++ packages/client/src/app/store.ts
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit'
-import authReducer, { Auth } from '../processes/auth/model/authSlice'
+import authReducer, { Auth } from '@processes/auth/model/authSlice'
 
 export interface Store {
   auth: Auth
