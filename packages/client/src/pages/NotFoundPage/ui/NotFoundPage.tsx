eb4862c2673d7f027df031eef4c420d569ed458d
cheriko <muglova@wr.company>
2024-10-21 22:27:52 +0300

    4 Вёрстка страницы ошибок 400

--- packages/client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ packages/client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,5 +1,17 @@
 import { FC } from 'react'
+import { useNavigate } from 'react-router-dom'
+import './NotFoundPage.scss'
 
 export const NotFoundPage: FC = () => {
-  return <div>404</div>
+  const navigate = useNavigate()
+  return (
+    <div className="error-page">
+      <h1 className="text-primary">404</h1>
+      <div className="error-page__subtitle">ой, страница не найдена</div>
+      <div className="error-page__text">
+        можно вернуться <a onClick={() => navigate(-1)}>назад</a> или на{' '}
+        <a href="/">главную</a>
+      </div>
+    </div>
+  )
 }
