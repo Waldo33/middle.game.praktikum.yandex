24e0b75621ebb37ec75526641466e2e9025ef9da
waldo <evgenijtser@gmail.com>
2024-10-21 21:23:36 +0300

    Add card class

---
+++ packages/client/src/widgets/Game/lib/CardClass.ts
@@ -0,0 +1,95 @@
+import { colors } from '../constants/colors'
+
+export class Card {
+  private id: number
+  private image: HTMLImageElement
+  private isRevealed = false
+  private isMatched = false
+  private isHovered = false
+
+  constructor(id: number, image: HTMLImageElement) {
+    this.id = id
+    this.image = image
+  }
+
+  public checkRevealed() {
+    return this.isRevealed
+  }
+
+  public checkMatched() {
+    return this.isMatched
+  }
+
+  public reveal() {
+    if (!this.isMatched) {
+      this.isHovered = false
+      this.isRevealed = true
+      this.animateFlip()
+    }
+  }
+
+  public hide() {
+    if (!this.isMatched) {
+      this.isRevealed = false
+      this.animateFlip()
+    }
+  }
+
+  public checkMatch(otherCard: Card): boolean {
+    if (this.id === otherCard.id) {
+      this.isMatched = true
+      return true
+    }
+    return false
+  }
+
+  private animateFlip() {
+    // TODO: Добавить анимированный переворот
+    return true
+  }
+
+  public highlight() {
+    if (!this.isRevealed) {
+      this.isHovered = true
+    }
+  }
+
+  public clearHighlight() {
+    if (!this.isRevealed) {
+      this.isHovered = false
+    }
+  }
+
+  public draw(
+    ctx: CanvasRenderingContext2D,
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  ) {
+    ctx.beginPath()
+    ctx.roundRect(x, y, width, height, 10)
+
+    if (this.isRevealed) {
+      ctx.fillStyle = colors.orange
+      ctx.strokeStyle = colors.orange
+    }
+
+    if (!this.isRevealed && this.isHovered) {
+      ctx.fillStyle = colors.darkblue
+      ctx.strokeStyle = colors.darkblue
+    }
+
+    if (!this.isRevealed && !this.isHovered) {
+      ctx.fillStyle = colors.blue
+      ctx.strokeStyle = colors.blue
+    }
+
+    ctx.stroke()
+    ctx.fill()
+
+    if (this.isRevealed) {
+      ctx.drawImage(this.image, x, y, width, height)
+    }
+  }
+}
