1b65b1ce2813f44cd3757ed2b6b79c9e89caeb58
waldo <evgenijtser@gmail.com>
2024-10-25 00:31:16 +0300

    Change getTimePad function

--- packages/client/src/pages/GamePage/lib/helpers/index.ts
+++ packages/client/src/pages/GamePage/lib/helpers/index.ts
@@ -1,11 +1,5 @@
-export const getTimePad = (timeInSec: number) => {
-  const minutes = Math.floor(timeInSec / 60)
-  const seconds = timeInSec - minutes * 60
-  return `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(
-    2,
-    '0'
-  )}`
-}
+export const getTimePad = (timeInSec: number) =>
+  new Date(timeInSec * 1000).toISOString().slice(14, 19)
 
 export const compareScoreWithLocalStorage = (newScore: number) => {
   const prevScore = Number(localStorage.getItem('score') || 0)
