102079a5e74347df0b975d568bcf88cd32f6cb59
cheriko <muglova@wr.company>
2024-10-27 16:30:58 +0300

    5 Вёрстка страницы ошибок 500, правки по ревью

--- packages/client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
+++ packages/client/src/pages/NotFoundPage/ui/NotFoundPage.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
+import { Button } from '@shared/components/ui/button'
 import '@shared/components/ui/ErrorPage.scss'
 
 export const NotFoundPage: FC = () => {
@@ -11,9 +12,9 @@ export const NotFoundPage: FC = () => {
       <div className="error-page__subtitle">ой, страница не найдена</div>
       <div className="error-page__text">
         можно вернуться{' '}
-        <Link to="/" onClick={() => navigate(-1)}>
+        <Button variant="link" onClick={() => navigate(-1)}>
           назад
-        </Link>{' '}
+        </Button>{' '}
         или на <Link to="/">главную</Link>
       </div>
     </div>
