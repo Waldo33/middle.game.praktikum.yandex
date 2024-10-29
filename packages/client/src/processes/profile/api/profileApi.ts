21d6a6d97216c1a38c3c540f4d4a63acdde17328
cheriko <muglova@wr.company>
2024-10-30 00:43:14 +0300

    13 Сверстать страницу пользователя, правки по ревью

--- packages/client/src/processes/profile/api/profileApi.ts
+++ packages/client/src/processes/profile/api/profileApi.ts
@@ -6,16 +6,18 @@ export interface Password {
 }
 
 export const changePassword = async (credentials: Password) => {
-  const response = await fetch(`${BASE_PROFILE_API}/password`, {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(credentials),
-    credentials: 'include',
-  })
-
-  if (!response.ok) {
-    const data = await response.json()
-    throw new Error(data.reason)
+  try {
+    const response = await fetch(`${BASE_PROFILE_API}/password`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(credentials),
+      credentials: 'include',
+    })
+    if (!response.ok) {
+      await response.json()
+    }
+  } catch (err) {
+    console.log(err)
   }
 
   return true
--- packages/client/src/widgets/avatar/avatarApi.ts
+++ packages/client/src/widgets/avatar/avatarApi.ts
@@ -1,16 +1,18 @@
 const BASE_AVATAR_API = `${import.meta.env.VITE_API_URL}/user`
 
 export const changeAvatar = async (data: FormData) => {
-  const response = await fetch(`${BASE_AVATAR_API}/profile/avatar`, {
-    method: 'PUT',
-    body: data,
-    credentials: 'include',
-    mode: 'cors',
-  })
-
-  if (!response.ok) {
-    const data = await response.json()
-    throw new Error(data.reason)
+  try {
+    const response = await fetch(`${BASE_AVATAR_API}/profile/avatar`, {
+      method: 'PUT',
+      body: data,
+      credentials: 'include',
+      mode: 'cors',
+    })
+    if (!response.ok) {
+      await response.json()
+    }
+  } catch (err) {
+    console.log(err)
   }
 
   return true
