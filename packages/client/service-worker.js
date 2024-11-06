e71d580a49e84cab703085bc770b71307f57daeb
waldo <evgenijtser@gmail.com>
2024-11-06 23:56:06 +0300

    Refactor service worker

--- packages/client/service-worker.js
+++ packages/client/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1';
+const CACHE_NAME = 'v2';
 
 const URLS_TO_CACHE = [
   '/',
@@ -14,54 +14,68 @@ const URLS_TO_CACHE = [
   '/favicon-dark.svg',
   '/favicon-light.svg',
   '/assets/',
+  '/assets/images/cards',
 ];
 
-this.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(URLS_TO_CACHE);
+const saveResourcesToCache = async (cacheName, urls) => {
+  try {
+    const cache = await caches.open(cacheName)
+    await cache.addAll(urls)
+  } catch (error) {
+    console.error('Не удалось закешировать ресурсы:', error)
+  }
+}
+
+const clearOldCache = async (cacheName) => {
+  try {
+    const cacheNames = await caches.keys();
+    return Promise.all(
+      cacheNames.map((cache) => {
+        if (cache !== cacheName) {
+          console.log('Удален старый кэш:', cache);
+          return caches.delete(cache);
+        }
       })
-      .catch((error) => console.error('Failed to cache resources:', error))
-  );
+    );
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const getResponseFromCacheOrResponseAndSave = async (event) => {
+  try {
+    const response = await caches.match(event.request);
+
+    if(response) {
+      return response;
+    }
+
+    const networkResponse = await fetch(event.request);
+
+    if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+      return networkResponse;
+    }
+
+    const responseToCache = networkResponse.clone();
+    const cache = await caches.open(CACHE_NAME);
+    await cache.put(event.request, responseToCache);
+
+    return networkResponse;
+  } catch (error) {
+    if (event.request.mode === 'navigate') {
+      return caches.match('/index.html');
+    }
+  }
+}
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(saveResourcesToCache(CACHE_NAME, URLS_TO_CACHE));
 });
 
-this.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
-            return caches.delete(cache);
-          }
-        })
-      );
-    })
-  );
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clearOldCache(CACHE_NAME));
 });
 
-this.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request)
-        .then((networkResponse) => {
-          if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
-            return networkResponse;
-          }
-
-          const responseToCache = networkResponse.clone();
-          caches.open(CACHE_NAME).then((cache) => {
-            cache.put(event.request, responseToCache);
-          });
-
-          return networkResponse;
-        });
-    }).catch(() => {
-      if (event.request.mode === 'navigate') {
-        return caches.match('/index.html');
-      }
-    })
-  );
+self.addEventListener('fetch', (event) => {
+  event.respondWith(getResponseFromCacheOrResponseAndSave(event));
 });
