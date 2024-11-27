029575ad9d11a04c6d48c4e296df539e24a1b66f
waldo <evgenijtser@gmail.com>
2024-11-27 18:25:03 +0300

    dispatch userThunk in useOAuthCodeHandle

--- packages/client/src/processes/auth/hooks/useOAuthCodeHandle.ts
+++ packages/client/src/processes/auth/hooks/useOAuthCodeHandle.ts
@@ -2,12 +2,17 @@ import { useSearchParams } from 'react-router-dom'
 import { signinWithOauthToken } from '../api/authApi'
 import { getCurrentHostWithPort } from '@shared/lib/getCurrentHostWithPort'
 import { useEffect } from 'react'
+import { userThunk } from '../model/thunks'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@app/store'
 
 export const useOAuthCodeHandle = () => {
+  const dispatch: AppDispatch = useDispatch()
   const [params, setParams] = useSearchParams()
 
   const signin = async (code: string) => {
     await signinWithOauthToken(code, getCurrentHostWithPort())
+    dispatch(userThunk())
     setParams('')
   }
 
