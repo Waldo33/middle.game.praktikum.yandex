d39f72762800a80cf768d8c1b71546e8d8a6a869
collusion <nicholas@markeaze.com>
2024-11-29 01:22:38 +0300

    delete chaining option

--- packages/client/src/app/App.tsx
+++ packages/client/src/app/App.tsx
@@ -1,14 +1,15 @@
+import React from 'react'
+
 import { Outlet, useLoaderData } from 'react-router-dom'
 
 import { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 
 import { Toaster } from '@shared/components/ui/toaster'
 import { AppDispatch } from './store'
 import { User, setUser, setError } from '@processes/auth/model/authSlice'
 
 import ErrorBoundary from '@shared/components/ErrorBoundary'
-import React from 'react'
 import { selectUser } from '@shared/model/selectors'
 import { userThunk } from '@processes/auth/model/thunks'
 import { PageInitArgs } from './router'
@@ -16,7 +17,7 @@ import { usePage } from '@shared/hooks/usePage'
 import { LoadingSpinner } from '@shared/components/ui/loading-spinner'
 import { useAuth } from '@shared/hooks/useAuth'
 
-export const App = () => {
+export const App: React.FC = () => {
   const dispatch: AppDispatch = useDispatch()
   const userData: User | null | unknown = useLoaderData()
   const { isFirstAuthCheck } = useAuth()
@@ -31,19 +32,24 @@ export const App = () => {
 
   usePage({ initPage: initAppPage })
 
-  return !isFirstAuthCheck ? (
-    <LoadingSpinner
-      style={{ position: 'absolute', top: '50%', left: '50%' }}
-      className="loader"
-      size={40}
-    />
-  ) : (
-    <div className="App">
-      <ErrorBoundary>
-        <Outlet />
-        <Toaster />
-      </ErrorBoundary>
-    </div>
+  return (
+    <>
+      {!isFirstAuthCheck && (
+        <LoadingSpinner
+          style={{ position: 'absolute', top: '50%', left: '50%' }}
+          className="loader"
+          size={40}
+        />
+      )}
+      {isFirstAuthCheck && (
+        <div className="App">
+          <ErrorBoundary>
+            <Outlet />
+            <Toaster />
+          </ErrorBoundary>
+        </div>
+      )}
+    </>
   )
 }
 
