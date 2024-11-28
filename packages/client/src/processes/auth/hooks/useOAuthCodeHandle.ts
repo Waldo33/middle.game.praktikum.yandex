9edfff523784cbf4d1ef8f4898f085482e4158d5
waldo <evgenijtser@gmail.com>
2024-11-28 21:39:56 +0300

    Refactor useOAuthCodeHandle hook

--- packages/client/src/processes/auth/hooks/useOAuthCodeHandle.ts
+++ packages/client/src/processes/auth/hooks/useOAuthCodeHandle.ts
@@ -5,22 +5,23 @@ import { useEffect } from 'react'
 import { userThunk } from '../model/thunks'
 import { useDispatch } from 'react-redux'
 import { AppDispatch } from '@app/store'
+import { useAuth } from '@shared/hooks/useAuth'
 
 export const useOAuthCodeHandle = () => {
   const dispatch: AppDispatch = useDispatch()
   const [params, setParams] = useSearchParams()
-
-  const signin = async (code: string) => {
-    await signinWithOauthToken(code, getCurrentHostWithPort())
-    dispatch(userThunk())
-    setParams('')
-  }
+  const isAuth = useAuth()
 
   useEffect(() => {
-    const oAuthCode = params.get('code')
+    const handleOAuthCode = async (code: string) => {
+      await signinWithOauthToken(code, getCurrentHostWithPort())
+      dispatch(userThunk())
+      setParams('')
+    }
 
-    if (oAuthCode) {
-      signin(oAuthCode)
+    const oAuthCode = params.get('code')
+    if (oAuthCode && !isAuth) {
+      handleOAuthCode(oAuthCode)
     }
   }, [params])
 }
