3f8631e45381317523e017ef07f4c468d9c5a1d3
cheriko <muglova@wr.company>
2024-12-02 18:47:23 +0300

    4 Добавить API для лидерборда, правки

--- packages/client/src/pages/GamePage/lib/helpers/index.ts
+++ packages/client/src/pages/GamePage/lib/helpers/index.ts
@@ -1,10 +1,7 @@
 export const getTimePad = (timeInSec: number) =>
   new Date(timeInSec * 1000).toISOString().slice(14, 19)
 
-export const compareScoreWithLocalStorage = (
-  key: `score-${number}` | 'bot-mode-score',
-  newScore: number
-) => {
+export const compareScoreWithLocalStorage = (key: string, newScore: number) => {
   const prevScore = Number(localStorage.getItem(key) || 0)
   if (prevScore < newScore) {
     localStorage.setItem(key, newScore.toString())
