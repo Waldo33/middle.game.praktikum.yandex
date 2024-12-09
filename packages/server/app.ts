0896a8fc122cc51877787abe14ccb6de312739a7
waldo <evgenijtser@gmail.com>
2024-12-10 01:04:54 +0300

    Add xss sanitize middleware

--- packages/server/app.ts
+++ packages/server/app.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import topicRoutes from './routes/topic'
+import { sanitizeMiddleware } from './middleware/xss'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(sanitizeMiddleware)
 
 app.use('/api/topics', topicRoutes)
 
